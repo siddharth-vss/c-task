1d3f8ac553c109901ef9a90e274844bcae652ccc
siddharth <131679411+siddharth-vss@users.noreply.github.com>
2025-01-29 16:19:22 +0530

    add thread program

---
+++ ConsoleApp/thread.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class ThreadExample
+    {
+        static void display()
+        {
+            for (int i = 1; i<= 10; i++)
+            {
+                Console.WriteLine($"process {i}");
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void Main(string[] args) {
+            ThreadStart th = new ThreadStart(ThreadExample.display);
+            Thread t = new Thread(th);
+            t.Start();
+        }
+    }
+}
